bc1aec5c6d84a64c8837571c57b1be52470a8195
Oscar Jiménez <fintech_mexa@gmail.com>
2020-12-14 22:55:45 -0300

    Primera carga, ejercicio clase

---
+++ Mexa IT company_archivos/config.js
@@ -0,0 +1,40 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "+56 9 81073760",
+        fax: "",
+        email: "info@mexa-it.com",
+        address: "AV. VITACURA 2939, oficina: 301, LAS CONDES",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "IT & Business solutions",
+            title: "Mexa IT company.",
+            subtitle: "Agile solutions and development.",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: ""
+        }
+    ]
+};
